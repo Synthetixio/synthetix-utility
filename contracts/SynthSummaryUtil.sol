b1a588419c95583f53c79a68f5c2c5d757cacdcd
Clement Balestrat <clement.balestrat@gmail.com>
2020-05-27 18:10:51 +1000

    added "synthsTotalSupplies" function

--- contracts/SynthSummaryUtil.sol
+++ contracts/SynthSummaryUtil.sol
@@ -1,65 +1,78 @@
 pragma solidity 0.4.25;
 
-
 contract ISynth {
     bytes32 public currencyKey;
-
-    function balanceOf(address owner) external view returns (uint256);
+    function balanceOf(address owner) external view returns (uint);
+    function totalSupply() external view returns (uint);
 }
-
-
 contract ISynthetix {
     ISynth[] public availableSynths;
-
-    function availableSynthCount() public view returns (uint256);
-
-    function availableCurrencyKeys() public view returns (bytes32[]);
+    function availableSynthCount() public view returns (uint);
+    function availableCurrencyKeys()
+        public
+        view
+        returns (bytes32[]);
 }
-
 
 contract IExchangeRates {
     function rateIsFrozen(bytes32 currencyKey) external view returns (bool);
-
-    function ratesForCurrencies(bytes32[] currencyKeys)
-        external
-        view
-        returns (uint256[] memory);
-
-    function effectiveValue(
-        bytes32 sourceCurrencyKey,
-        uint256 sourceAmount,
-        bytes32 destinationCurrencyKey
-    ) public view returns (uint256);
+    function ratesForCurrencies(bytes32[] currencyKeys) external view returns (uint[] memory);
+    function effectiveValue(bytes32 sourceCurrencyKey, uint sourceAmount, bytes32 destinationCurrencyKey)
+    public
+    view
+    returns (uint);
 }
-
 
 contract SynthSummaryUtil {
     ISynthetix public synthetix;
     IExchangeRates public exchangeRates;
-
     constructor(address _synthetix, address _exchangeRates) public {
         synthetix = ISynthetix(_synthetix);
         exchangeRates = IExchangeRates(_exchangeRates);
     }
 
-    function totalSynthsInKey(address account, bytes32 currencyKey)
-        external
-        view
-        returns (uint256 total)
-    {
-        uint256 numSynths = synthetix.availableSynthCount();
-        for (uint256 i = 0; i < numSynths; i++) {
+    function totalSynthsInKey(address account, bytes32 currencyKey) external view returns (uint total) {
+        uint numSynths = synthetix.availableSynthCount();
+        for (uint i = 0; i < numSynths; i++) {
             ISynth synth = synthetix.availableSynths(i);
-            total += exchangeRates.effectiveValue(
-                synth.currencyKey(),
-                synth.balanceOf(account),
-                currencyKey
-            );
+            total += exchangeRates.effectiveValue(synth.currencyKey(), synth.balanceOf(account), currencyKey);
         }
         return total;
     }
 
-    function synthsBalances(address account)
+    function synthsBalances(address account) external view returns (bytes32[], uint[],  uint[]) {
+        uint numSynths = synthetix.availableSynthCount();
+        bytes32[] memory currencyKeys = new bytes32[](numSynths);
+        uint[] memory balances = new uint[](numSynths);
+        uint[] memory sUSDBalances = new uint[](numSynths);
+        for (uint i = 0; i < numSynths; i++) {
+            ISynth synth = synthetix.availableSynths(i);
+            currencyKeys[i] = synth.currencyKey();
+            balances[i] = synth.balanceOf(account);
+            sUSDBalances[i] = exchangeRates.effectiveValue(synth.currencyKey(), synth.balanceOf(account), 'sUSD');
+        }
+        return (currencyKeys, balances, sUSDBalances);
+    }
+
+    function frozenSynths() external view returns (bytes32[]) {
+        uint numSynths = synthetix.availableSynthCount();
+        bytes32[] memory frozenSynthsKeys = new bytes32[](numSynths);
+        for (uint i = 0; i < numSynths; i++) {
+            ISynth synth = synthetix.availableSynths(i);
+            if (exchangeRates.rateIsFrozen(synth.currencyKey())) {
+                frozenSynthsKeys[i] = synth.currencyKey();
+            }
+
+        }
+        return frozenSynthsKeys;
+    }
+
+    function synthsRates() external view returns (bytes32[], uint[]) {
+        bytes32[] memory currencyKeys = synthetix.availableCurrencyKeys();
+        return (currencyKeys, exchangeRates.ratesForCurrencies(currencyKeys));
+    }
+
+    function synthsTotalSupplies()
         external
         view
         returns (bytes32[], uint256[], uint256[])
@@ -71,45 +84,13 @@ contract SynthSummaryUtil {
         for (uint256 i = 0; i < numSynths; i++) {
             ISynth synth = synthetix.availableSynths(i);
             currencyKeys[i] = synth.currencyKey();
-            balances[i] = synth.balanceOf(account);
+            balances[i] = synth.totalSupply();
             sUSDBalances[i] = exchangeRates.effectiveValue(
                 synth.currencyKey(),
-                synth.balanceOf(account),
+                synth.totalSupply(),
                 "sUSD"
             );
         }
         return (currencyKeys, balances, sUSDBalances);
-    }
-
-    function frozenSynths() external view returns (bytes32[]) {
-        uint256 numSynths = synthetix.availableSynthCount();
-        bytes32[] memory frozenSynthsKeys = new bytes32[](numSynths);
-        for (uint256 i = 0; i < numSynths; i++) {
-            ISynth synth = synthetix.availableSynths(i);
-            if (exchangeRates.rateIsFrozen(synth.currencyKey())) {
-                frozenSynthsKeys[i] = synth.currencyKey();
-            }
-        }
-        return frozenSynthsKeys;
-    }
-
-    function synthsRates() external view returns (bytes32[], uint256[]) {
-        bytes32[] memory currencyKeys = synthetix.availableCurrencyKeys();
-        return (currencyKeys, exchangeRates.ratesForCurrencies(currencyKeys));
-    }
-
-    function totalSynthsInKeyForAccounts(address[] accounts, bytes32 currencyKey)
-        external
-        view
-        returns (uint256[])
-    {
-        uint256 memory numAccounts = accounts.length;
-        uint256[] memory accountsTotal = new uint256[](numAccounts)
-        for (uint256 i = 0; i < numAccounts; i++) {
-            address account = accounts[i];
-            uint256 total = totalSynthsInKey(account, currencyKey);
-            accountsTotal[i] = total;
-        }
-        return accountsTotal;
     }
 }
