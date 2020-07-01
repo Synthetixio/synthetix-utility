35da6f048f2827d458c95768215122bb18382713
Anton Jurisevic <zyzek@users.noreply.github.com>
2020-07-01 11:31:59 +1000

    Updated the utility contract to use the address resolver.

--- contracts/SynthSummaryUtil.sol
+++ contracts/SynthSummaryUtil.sol
@@ -1,37 +1,91 @@
-pragma solidity 0.4.25;
+pragma solidity ^0.5.16;
 
-contract ISynth {
-    bytes32 public currencyKey;
+interface ISynth {
+    function currencyKey() external view returns (bytes32);
     function balanceOf(address owner) external view returns (uint);
     function totalSupply() external view returns (uint);
 }
-contract ISynthetix {
-    ISynth[] public availableSynths;
-    function availableSynthCount() public view returns (uint);
-    function availableCurrencyKeys()
-        public
-        view
-        returns (bytes32[]);
+interface ISynthetix {
+    function availableSynths(uint index) external view returns (ISynth);
+    function availableSynthCount() external view returns (uint);
+    function availableCurrencyKeys() external view returns (bytes32[] memory);
 }
 
-contract IExchangeRates {
+interface IExchangeRates {
     function rateIsFrozen(bytes32 currencyKey) external view returns (bool);
-    function ratesForCurrencies(bytes32[] currencyKeys) external view returns (uint[] memory);
+    function ratesForCurrencies(bytes32[] calldata currencyKeys) external view returns (uint[] memory);
     function effectiveValue(bytes32 sourceCurrencyKey, uint sourceAmount, bytes32 destinationCurrencyKey)
-    public
+    external
     view
     returns (uint);
 }
 
-contract SynthSummaryUtil {
-    ISynthetix public synthetix;
-    IExchangeRates public exchangeRates;
-    constructor(address _synthetix, address _exchangeRates) public {
-        synthetix = ISynthetix(_synthetix);
-        exchangeRates = IExchangeRates(_exchangeRates);
+interface IAddressResolver {
+    function getAddress(bytes32 name) external view returns (address);
+    function getSynth(bytes32 key) external view returns (address);
+    function requireAndGetAddress(bytes32 name, string calldata reason) external view returns (address);
+}
+
+
+contract Owned {
+    address public owner;
+    address public nominatedOwner;
+
+    constructor(address _owner) public {
+        require(_owner != address(0), "Owner address cannot be 0");
+        owner = _owner;
+        emit OwnerChanged(address(0), _owner);
+    }
+
+    function nominateNewOwner(address _owner) external onlyOwner {
+        nominatedOwner = _owner;
+        emit OwnerNominated(_owner);
+    }
+
+    function acceptOwnership() external {
+        require(msg.sender == nominatedOwner, "You must be nominated before you can accept ownership");
+        emit OwnerChanged(owner, nominatedOwner);
+        owner = nominatedOwner;
+        nominatedOwner = address(0);
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the contract owner may perform this action");
+        _;
+    }
+
+    event OwnerNominated(address newOwner);
+    event OwnerChanged(address oldOwner, address newOwner);
+}
+
+
+contract SynthSummaryUtil is Owned {
+
+    IAddressResolver public addressResolver;
+
+    bytes32 internal constant CONTRACT_SYNTHETIX = "Synthetix";
+    bytes32 internal constant CONTRACT_EXRATES = "ExchangeRates";
+    bytes32 internal constant SUSD = "sUSD";
+
+    constructor(address resolver) public Owned(msg.sender) {
+        addressResolver = IAddressResolver(resolver);
+    }
+
+    function _synthetix() internal view returns (ISynthetix) {
+        return ISynthetix(addressResolver.requireAndGetAddress(CONTRACT_SYNTHETIX, "Missing Synthetix address"));
+    }
+
+    function _exchangeRates() internal view returns (IExchangeRates) {
+        return IExchangeRates(addressResolver.requireAndGetAddress(CONTRACT_EXRATES, "Missing ExchangeRates address"));
+    }
+
+    function setAddressResolver(IAddressResolver resolver) external onlyOwner {
+        addressResolver = resolver;
     }
 
     function totalSynthsInKey(address account, bytes32 currencyKey) external view returns (uint total) {
+        ISynthetix synthetix = _synthetix();
+        IExchangeRates exchangeRates = _exchangeRates();
         uint numSynths = synthetix.availableSynthCount();
         for (uint i = 0; i < numSynths; i++) {
             ISynth synth = synthetix.availableSynths(i);
@@ -40,7 +94,9 @@ contract SynthSummaryUtil {
         return total;
     }
 
-    function synthsBalances(address account) external view returns (bytes32[], uint[],  uint[]) {
+    function synthsBalances(address account) external view returns (bytes32[] memory, uint[] memory,  uint[] memory) {
+        ISynthetix synthetix = _synthetix();
+        IExchangeRates exchangeRates = _exchangeRates();
         uint numSynths = synthetix.availableSynthCount();
         bytes32[] memory currencyKeys = new bytes32[](numSynths);
         uint[] memory balances = new uint[](numSynths);
@@ -49,12 +105,14 @@ contract SynthSummaryUtil {
             ISynth synth = synthetix.availableSynths(i);
             currencyKeys[i] = synth.currencyKey();
             balances[i] = synth.balanceOf(account);
-            sUSDBalances[i] = exchangeRates.effectiveValue(synth.currencyKey(), synth.balanceOf(account), 'sUSD');
+            sUSDBalances[i] = exchangeRates.effectiveValue(currencyKeys[i], balances[i], SUSD);
         }
         return (currencyKeys, balances, sUSDBalances);
     }
 
-    function frozenSynths() external view returns (bytes32[]) {
+    function frozenSynths() external view returns (bytes32[] memory) {
+        ISynthetix synthetix = _synthetix();
+        IExchangeRates exchangeRates = _exchangeRates();
         uint numSynths = synthetix.availableSynthCount();
         bytes32[] memory frozenSynthsKeys = new bytes32[](numSynths);
         for (uint i = 0; i < numSynths; i++) {
@@ -67,16 +125,19 @@ contract SynthSummaryUtil {
         return frozenSynthsKeys;
     }
 
-    function synthsRates() external view returns (bytes32[], uint[]) {
-        bytes32[] memory currencyKeys = synthetix.availableCurrencyKeys();
-        return (currencyKeys, exchangeRates.ratesForCurrencies(currencyKeys));
+    function synthsRates() external view returns (bytes32[] memory, uint[] memory) {
+        bytes32[] memory currencyKeys = _synthetix().availableCurrencyKeys();
+        return (currencyKeys, _exchangeRates().ratesForCurrencies(currencyKeys));
     }
 
     function synthsTotalSupplies()
         external
         view
-        returns (bytes32[], uint256[], uint256[])
+        returns (bytes32[] memory, uint256[] memory, uint256[] memory)
     {
+        ISynthetix synthetix = _synthetix();
+        IExchangeRates exchangeRates = _exchangeRates();
+
         uint256 numSynths = synthetix.availableSynthCount();
         bytes32[] memory currencyKeys = new bytes32[](numSynths);
         uint256[] memory balances = new uint256[](numSynths);
@@ -86,9 +147,9 @@ contract SynthSummaryUtil {
             currencyKeys[i] = synth.currencyKey();
             balances[i] = synth.totalSupply();
             sUSDBalances[i] = exchangeRates.effectiveValue(
-                synth.currencyKey(),
-                synth.totalSupply(),
-                "sUSD"
+                currencyKeys[i],
+                balances[i],
+                SUSD
             );
         }
         return (currencyKeys, balances, sUSDBalances);
